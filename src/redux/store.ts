8f6eb2de79f8e1148338c353b330f90c405e24e6
Liber <libermenna@gmail.com>
2021-05-13 10:19:29 -0300

    FIX: removed composeWitHDevTools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,13 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root.reducer';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  applyMiddleware(thunk),
 );
 
 export default store;
